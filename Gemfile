dc5f94fd30fcde441948f2b913caa64e1b132574
Alan Gabbianelli <alan.gabbianelli@digital.cabinet-office.gov.uk>
2021-03-08 15:13:14 +0000

    Add useful gems commonly used in GOV.UK projects
    Trello card: https://trello.com/c/ZeAKl2SM/651-set-up-a-new-app

--- Gemfile
+++ Gemfile
@@ -8,9 +8,26 @@ ruby File.read(".ruby-version").strip
 gem "rails", "6.1.3"
 
 gem "bootsnap"
+gem "gds-sso"
+gem "govuk_app_config"
 gem "pg"
 gem "puma"
 
+group :development, :test do
+  gem "awesome_print"
+  gem "factory_bot_rails"
+  gem "govuk_test"
+  gem "pry-byebug"
+  gem "pry-rails"
+  gem "rspec-rails"
+  gem "rubocop-govuk"
+end
+
 group :development do
   gem "listen"
+end
+
+group :test do
+  gem "simplecov"
+  gem "webmock"
 end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+ENV["RAILS_ENV"] ||= "test"
+
+require "simplecov"
+SimpleCov.start "rails"
+
+require File.expand_path("../config/environment", __dir__)
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require "rspec/rails"
+require "webmock/rspec"
+
+Dir[Rails.root.join("spec", "support", "**", "*.rb")].sort.each { |f| require f }
+GovukTest.configure
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  puts e.to_s.strip
+  exit 1
+end
+
+RSpec.configure do |config|
+  config.expose_dsl_globally = false
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.example_status_persistence_file_path = "spec/examples.txt"
+  config.disable_monkey_patching!
+  config.default_formatter = "doc" if config.files_to_run.one?
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+end
