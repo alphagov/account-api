8366941ce0c7b3ed11bb84b5ebbd454d0568b369
Michael Walker <mike@barrucadu.co.uk>
2021-03-10 13:35:41 +0000

    Add placeholder authentication controller

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,7 @@
+class AuthenticationController < ApplicationController
+  def sign_in; end
+
+  def callback; end
+
+  def create_state; end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   get "/healthcheck", to: GovukHealthcheck.rack_response(
     GovukHealthcheck::ActiveRecord,
   )
+
+  scope :api do
+    scope :oauth2 do
+      get "/sign-in", to: "authentication#sign_in"
+      post "/callback", to: "authentication#callback"
+      post "/state", to: "authentication#create_state"
+    end
+  end
 end
