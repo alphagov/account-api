c081d442519897723f89a48832eb9fb61020ebd5
Michael Walker <mike@barrucadu.co.uk>
2021-03-11 12:14:45 +0000

    Add validations on the redirect_path

--- app/models/auth_request.rb
+++ app/models/auth_request.rb
@@ -2,6 +2,8 @@ class AuthRequest < ApplicationRecord
   EXPIRATION_AGE = 2.hours
   scope :expired, -> { where("created_at < ?", EXPIRATION_AGE.ago) }
 
+  validate :redirect_path_is_safe
+
   # This has to be something which the account manager can use to retrieve a JWT, if there is one:
   # https://github.com/alphagov/govuk-account-manager-prototype/blob/6a68e02055fe3c70083b3899b474b10cc944ffa5/config/initializers/doorkeeper.rb#L14
   def to_oauth_state
@@ -13,5 +15,20 @@ class AuthRequest < ApplicationRecord
     return nil unless bits.length == 2
 
     find_by(id: bits[1], oauth_state: bits[0])
+  end
+
+  def redirect_path_is_safe
+    return if redirect_path.nil?
+    return if redirect_path.empty?
+
+    if redirect_path&.starts_with? "//"
+      errors.add(:redirect_path, "can't be protocol-relative")
+      return
+    end
+
+    return if redirect_path.starts_with? "/"
+    return if redirect_path.starts_with?("http://") && Rails.env.development?
+
+    errors.add(:redirect_path, "can't be absolute")
   end
 end
--- spec/models/auth_request_spec.rb
+++ spec/models/auth_request_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe AuthRequest do
     request1 = described_class.create!(
       oauth_state: SecureRandom.alphanumeric(10),
       oidc_nonce: SecureRandom.alphanumeric(10),
-      redirect_path: SecureRandom.alphanumeric(10),
+      redirect_path: "/" + SecureRandom.alphanumeric(10),
     )
     request2 = described_class.create!(
       oauth_state: request1.oauth_state,
@@ -13,5 +13,23 @@ RSpec.describe AuthRequest do
 
     expect(described_class.from_oauth_state(request1.to_oauth_state).id).to eq(request1.id)
     expect(described_class.from_oauth_state(request2.to_oauth_state).id).to eq(request2.id)
+  end
+
+  it "allows an empty redirect" do
+    expect(described_class.new(oauth_state: "foo", oidc_nonce: "bar", redirect_path: "")).to be_valid
+    expect(described_class.new(oauth_state: "foo", oidc_nonce: "bar", redirect_path: nil)).to be_valid
+  end
+
+  it "allows path-relative redirects" do
+    expect(described_class.new(oauth_state: "foo", oidc_nonce: "bar", redirect_path: "/good-page")).to be_valid
+  end
+
+  it "forbids protocol-relative redirects" do
+    expect(described_class.new(oauth_state: "foo", oidc_nonce: "bar", redirect_path: "//malicious-site.com")).not_to be_valid
+  end
+
+  it "forbids absolute redirects" do
+    expect(described_class.new(oauth_state: "foo", oidc_nonce: "bar", redirect_path: "http://malicious-site.com")).not_to be_valid
+    expect(described_class.new(oauth_state: "foo", oidc_nonce: "bar", redirect_path: "https://malicious-site.com")).not_to be_valid
   end
 end
