c4a4f81d9634cc0cbcbe337094cd7b188cf77192
Michael Walker <mike@barrucadu.co.uk>
2021-04-14 10:14:59 +0100

    Encrypt & sign session identifiers
    We're going to introduce a new level of authentication (LoA) to the
    account, and so we want to track which LoA a user logged in with so
    that the account-api can reject requests where the user isn't
    authenticated enough.
    
    This is so that we can:
    
    - require that the user has done (or had remembered) MFA when they use
      the Transition Checker;
    
    - but NOT require MFA to save pages, where it's too heavyweight.
    
    We want to store the LoA in the session header, so we don't need to
    query the auth service on every request.  But we don't want the user
    to be able to tamper with it, so we now need to sign the header.
    
    For future-proofing, I've gone for storing a JSON object in the
    session, rather than a comma-separated string.  It'll be easier to add
    new fields this way.
    
    The encoding is:
    
        (1) urlsafebase64(
        (2)   encrypt(
                {
                  access_token: "access_token",
                  refresh_token: "refresh_token",
        (3)     }.to_json,
              )
            )
    
    1. We're storing this in a header, so it needs to be a URL-safe value
    2. It's encrypted & signed
    3. We turn the hash into a JSON string, because encryption operates on
       strings
    
    To avoid booting out all current users, the legacy unsigned sessions
    are accepted for now (and are upgraded into signed sessions).  Those
    will be removed once we need to have LoA in the session.
    
    Note: the account-api checking LoA is NOT how LoA will be done for
    attributes in the Digital Identity-run attribute store.  If they want
    to have attributes which need different LoAs to access, they will
    implement that themselves.  It would be a security breach waiting to
    happen if they asked every service to independently implement access
    checks.  This change is *just* for GOV.UK attributes.
    
    But later cards about handling 403 errors and requesting a specific
    LoA when authenticating do move us towards being able to work with a
    multi-LoA Digital Identity-run attribute store.

--- app/helpers/session_header_helper.rb
+++ app/helpers/session_header_helper.rb
@@ -1,9 +1,26 @@
 module SessionHeaderHelper
   def to_account_session(access_token, refresh_token)
-    "#{Base64.urlsafe_encode64(access_token)}.#{Base64.urlsafe_encode64(refresh_token)}"
+    SessionEncryptor
+      .new(session_signing_key: Rails.application.secrets.session_signing_key)
+      .encrypt_session(
+        access_token: access_token,
+        refresh_token: refresh_token,
+      )
   end
 
   def from_account_session(govuk_account_session)
+    return if govuk_account_session.blank?
+
+    from_signed_account_session(govuk_account_session) || from_legacy_account_session(govuk_account_session)
+  end
+
+  def from_signed_account_session(govuk_account_session)
+    SessionEncryptor
+      .new(session_signing_key: Rails.application.secrets.session_signing_key)
+      .decrypt_session(govuk_account_session)
+  end
+
+  def from_legacy_account_session(govuk_account_session)
     bits = (govuk_account_session || "").split(".")
     if bits.length == 2
       {
---
+++ spec/lib/session_encryptor_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe SessionEncryptor do
+  let(:key) { "encryption-key" }
+  let(:encoded) { described_class.new(session_signing_key: key).encrypt_session(access_token: "access-token", refresh_token: "refresh-token") }
+
+  it "rejects if the salt has been tampered with" do
+    bits = Base64.urlsafe_decode64(encoded).split("$$")
+    salt = bits[0]
+    message = bits[1]
+    salt[0] = "X"
+    new_encoded = Base64.urlsafe_encode64("#{salt}$$#{message}")
+
+    expect(described_class.new(session_signing_key: key).decrypt_session(new_encoded)).to be_nil
+  end
+
+  it "rejects if the salt is missing" do
+    message = Base64.urlsafe_decode64(encoded).split("$$")[1]
+    new_encoded = Base64.urlsafe_encode64(message)
+
+    expect(described_class.new(session_signing_key: key).decrypt_session(new_encoded)).to be_nil
+  end
+
+  it "rejects if the session has been signed with a different key" do
+    expect(described_class.new(session_signing_key: "a-different-key").decrypt_session(encoded)).to be_nil
+  end
+
+  it "rejects if the session is invalid base64" do
+    expect(described_class.new(session_signing_key: key).decrypt_session("?")).to be_nil
+    expect(described_class.new(session_signing_key: key).decrypt_session("")).to be_nil
+    expect(described_class.new(session_signing_key: key).decrypt_session(nil)).to be_nil
+  end
+end
