4d9599c335d4830ef407157a985ec60e2e921722
Michael Walker <mike@barrucadu.co.uk>
2021-05-25 13:53:09 +0100

    Use the correct pact branch

--- spec/service_consumers/pact_helper.rb
+++ spec/service_consumers/pact_helper.rb
@@ -22,7 +22,7 @@ Pact.service_provider "Account API" do
     else
       base_url = "https://pact-broker.cloudapps.digital"
       path = "pacts/provider/#{url_encode(name)}/consumer/#{url_encode(consumer_name)}"
-      version_modifier = "versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'master'))}"
+      version_modifier = "versions/#{url_encode(ENV.fetch('PACT_CONSUMER_VERSION', 'branch-master'))}"
 
       pact_uri("#{base_url}/#{path}/#{version_modifier}")
     end
