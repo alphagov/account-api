75f9e633076377381c4f9b2e283574497e50bff1
Michael Walker <mike@barrucadu.co.uk>
2021-06-09 16:02:17 +0100

    Revert "Remove duplication of absolute path validation"

--- app/models/saved_page.rb
+++ app/models/saved_page.rb
@@ -1,7 +1,9 @@
 class SavedPage < ApplicationRecord
   belongs_to :oidc_user
 
-  validates :page_path, uniqueness: { scope: :oidc_user_id }, presence: true, absolute_path: true
+  validates :page_path, uniqueness: { scope: :oidc_user_id }, presence: true
+
+  validate :page_path_is_valid_path
 
   def to_hash
     {
@@ -18,5 +20,17 @@ class SavedPage < ApplicationRecord
       title: content_item["title"],
       public_updated_at: content_item["public_updated_at"] ? Time.zone.parse(content_item["public_updated_at"]) : nil,
     }
+  end
+
+private
+
+  def page_path_is_valid_path
+    errors.add(:page_path, :invalid_path, message: "must only include URL path") unless is_valid_path?
+  end
+
+  def is_valid_path?
+    page_path&.starts_with?("/") && URI(page_path).path == page_path
+  rescue StandardError
+    false
   end
 end
