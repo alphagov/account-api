9c2cacd2483c137836f5811accec1a72ea65a457
Alex Whitehead-Smith <alex.smith@digital.cabinet-office.gov.uk>
2021-09-17 11:40:24 +0100

    Add a rake task to check if a user exists
    Look up users by email address to see if they have an account. We have
    to do this occasionally, so add a more convenient method than using the
    Rails console.

---
+++ spec/lib/support_spec.rb
@@ -0,0 +1,31 @@
+Rails.application.load_tasks
+
+RSpec.describe "Support tasks" do
+  describe ":find_user" do
+    subject(:task) { Rake.application["support:find_user"] }
+
+    context "when there are no users" do
+      it "outputs the user doesn't exist" do
+        expect { task.execute({ email: "foo@example.gov.uk" }) }.to output("User 'foo@example.gov.uk' does not exist\n").to_stdout
+      end
+    end
+
+    context "when a user with email 'foo@example.gov.uk' exists" do
+      before do
+        FactoryBot.create(:oidc_user, email: "foo@example.gov.uk")
+      end
+
+      it "can find that user by email" do
+        expect { task.execute({ email: "foo@example.gov.uk" }) }.to output("User 'foo@example.gov.uk' exists\n").to_stdout
+      end
+
+      it "outputs other users don't exist" do
+        expect { task.execute({ email: "bar@example.com" }) }.to output("User 'bar@example.com' does not exist\n").to_stdout
+      end
+
+      it "performs a lowercase search for the user" do
+        expect { task.execute({ email: "FOO@Example.Gov.uK" }) }.to output("User 'FOO@Example.Gov.uK' exists\n").to_stdout
+      end
+    end
+  end
+end
