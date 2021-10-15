184fd3d23cbbd5a388eb97e59b81109c3ef0dabc
Michael Walker <mike@barrucadu.co.uk>
2021-10-15 14:28:07 +0100

    Import the sorting hat page content & add publishing task
    We want to publish a content item with actual content in it, rather
    than just a special route, so the page will show up in search results.
    
    Co-authored-by: Dana Cotoran <dana.cotoran@gmail.com>

--- lib/publishing_api_tasks.rb
+++ lib/publishing_api_tasks.rb
@@ -7,6 +7,44 @@ class PublishingApiTasks
     @content_items = content_items || YAML.safe_load(File.read(Rails.root.join("config/content_items.yml"))).deep_symbolize_keys
   end
 
+  def publish_help_page(name)
+    content_item = @content_items[:help_pages].fetch(name.to_sym)
+    content_id = content_item.fetch(:content_id)
+    base_path = content_item.fetch(:base_path)
+
+    claim_path base_path
+
+    @logger.info("Publishing content for #{base_path}")
+    @publishing_api.put_content(
+      content_id,
+      {
+        update_type: "major",
+        document_type: "help_page",
+        schema_name: "help_page",
+        publishing_app: "account-api",
+        rendering_app: content_item.fetch(:rendering_app),
+        base_path: base_path,
+        title: content_item.fetch(:title),
+        description: content_item.fetch(:description),
+        details: {
+          body: [
+            {
+              content_type: "text/html",
+              content: ActionController::Base.render(template: "help_pages/#{name}"),
+            },
+          ],
+        },
+        routes: [
+          {
+            path: content_item.fetch(:base_path),
+            type: "exact",
+          },
+        ],
+      },
+    )
+    @publishing_api.publish(content_id, "major")
+  end
+
   def publish_special_routes
     publisher = GdsApi::PublishingApi::SpecialRoutePublisher.new(
       publishing_api: @publishing_api,
--- spec/lib/publishing_api_tasks_spec.rb
+++ spec/lib/publishing_api_tasks_spec.rb
@@ -10,6 +10,32 @@ RSpec.describe PublishingApiTasks do
   let(:logger) { instance_double("Logger") }
   let(:content_items) { nil }
 
+  describe "#publish_help_page" do
+    before { allow(logger).to receive(:info) }
+
+    let(:help_page_name) { "sign_in" }
+    let(:help_page) { { content_id: SecureRandom.uuid, base_path: "/foo", title: "title", description: "description", rendering_app: "frontend" } }
+    let(:content_items) { { help_pages: { help_page_name.to_sym => help_page } } }
+
+    it "takes ownership of the route and publishes the content item" do
+      stub_claim_path = stub_path_reservation(help_page[:base_path])
+      stub_put_content = stub_publishing_api_put_content(
+        help_page[:content_id],
+        hash_including(help_page.merge(publishing_app: "account-api").except(:content_id)),
+      )
+      stub_publish = stub_publishing_api_publish(
+        help_page[:content_id],
+        hash_including(update_type: "major"),
+      )
+
+      tasks.publish_help_page(help_page_name)
+
+      expect(stub_claim_path).to have_been_made
+      expect(stub_put_content).to have_been_made
+      expect(stub_publish).to have_been_made
+    end
+  end
+
   describe "#publish_special_routes" do
     before { allow(logger).to receive(:info) }
 
