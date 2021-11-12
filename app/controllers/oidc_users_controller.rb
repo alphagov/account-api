0ec031479ccb16e4089e687e5cd62b8296ed617f
Michael Walker <mike@barrucadu.co.uk>
2021-11-12 11:48:19 +0000

    Refactor: split up OidcUsers controller methods
    The email-alert-api handling bits of these were getting a bit long, so
    extract them into methods with descriptive names.

--- app/controllers/oidc_users_controller.rb
+++ app/controllers/oidc_users_controller.rb
@@ -16,40 +16,7 @@ class OidcUsersController < ApplicationController
     end
 
     if (email_changed || email_verified_changed) && user.email && user.email_verified
-      begin
-        # if the user has linked their notifications account to their
-        # GOV.UK account we don't need to update their
-        # `user.email_subscriptions`, because we can update the
-        # subscriber directly.
-        subscriber_id = GdsApi.email_alert_api
-          .find_subscriber_by_govuk_account(govuk_account_id: user.id)
-          .dig("subscriber", "id")
-        GdsApi.email_alert_api.change_subscriber(
-          id: subscriber_id,
-          new_address: user.email,
-          on_conflict: "merge",
-        )
-      rescue GdsApi::HTTPNotFound
-        # but for users who haven't linked their notifications account
-        # to their GOV.UK account, we do need to update any
-        # account-linked subscriptions they have (eg, brexit checker
-        # users who haven't touched notifications since registering
-        # through the checker).
-        #
-        # this branch can be removed once we have no GOV.UK accounts
-        # which have subscriptions but are *not* linked to the
-        # corresponding notifications account.
-        user.email_subscriptions.find_each do |subscription|
-          if subscription.activated? && !subscription.check_if_still_active!
-            subscription.destroy!
-            next
-          end
-
-          subscription.reactivate_if_confirmed!
-        rescue EmailSubscription::SubscriberListNotFound
-          subscription.destroy!
-        end
-      end
+      update_email_alert_api_address(user)
     end
 
     render json: user.get_attributes_by_name(OIDC_USER_ATTRIBUTES).merge(sub: user.sub)
@@ -61,14 +28,7 @@ class OidcUsersController < ApplicationController
       legacy_sub: params[:legacy_sub],
     )
 
-    begin
-      subscriber_id = GdsApi.email_alert_api
-        .find_subscriber_by_govuk_account(govuk_account_id: user.id)
-        .dig("subscriber", "id")
-      GdsApi.email_alert_api.unsubscribe_subscriber(subscriber_id)
-    rescue GdsApi::HTTPNotFound
-      nil
-    end
+    end_email_alert_api_subscriptions(user)
 
     user.destroy!
 
@@ -81,5 +41,52 @@ private
 
   def authorise_sso_user!
     authorise_user!("update_protected_attributes")
+  end
+
+  def update_email_alert_api_address(user)
+    # if the user has linked their notifications account to their
+    # GOV.UK account we don't need to update their
+    # `user.email_subscriptions`, because we can update the subscriber
+    # directly.
+    GdsApi.email_alert_api.change_subscriber(
+      id: email_alert_api_subscriber_id(user),
+      new_address: user.email,
+      on_conflict: "merge",
+    )
+  rescue GdsApi::HTTPNotFound
+    # but for users who haven't linked their notifications account to
+    # their GOV.UK account, we do need to update any account-linked
+    # subscriptions they have (eg, brexit checker users who haven't
+    # touched notifications since registering through the checker).
+    #
+    # this branch can be removed once we have no GOV.UK accounts which
+    # have subscriptions but are *not* linked to the corresponding
+    # notifications account.
+    user.email_subscriptions.find_each do |subscription|
+      if subscription.activated? && !subscription.check_if_still_active!
+        subscription.destroy!
+        next
+      end
+
+      subscription.reactivate_if_confirmed!
+    rescue EmailSubscription::SubscriberListNotFound
+      subscription.destroy!
+    end
+  end
+
+  def end_email_alert_api_subscriptions(user)
+    GdsApi.email_alert_api.unsubscribe_subscriber(
+      email_alert_api_subscriber_id(user),
+    )
+  rescue GdsApi::HTTPNotFound
+    # if there is no linked notifications account, there is nothing to
+    # do
+    nil
+  end
+
+  def email_alert_api_subscriber_id(user)
+    GdsApi.email_alert_api
+      .find_subscriber_by_govuk_account(govuk_account_id: user.id)
+      .dig("subscriber", "id")
   end
 end
