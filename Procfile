53c59a57710f0ea8bebfcc781926efc470c04de5
Michael Walker <mike@barrucadu.co.uk>
2022-01-31 11:36:52 +0000

    Remove publishing message processor
    This has been removed from puppet.

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: bundle exec unicorn -c ./config/unicorn.rb -p ${PORT:-3000}
 worker: bundle exec sidekiq -C ./config/sidekiq.yml
-publishing-queue-listener: bundle exec rake message_queue:consumer
--- lib/message_queue_processor.rb
+++
@@ -1,6 +0,0 @@
-# TODO: revisit whether we'll need to consume publishing updates after implementing single-page notifications
-class MessageQueueProcessor
-  def process(message)
-    message.ack
-  end
-end
