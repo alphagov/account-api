b0ada146ecf77a2432d50e5a14b9d60175a7dc1f
Edward Kerry <edward.kerry@digital.cabinet-office.gov.uk>
2023-01-18 15:25:48 +0000

    Add Rake task to delete user account by email address

--- lib/tasks/support.rake
+++ lib/tasks/support.rake
@@ -7,4 +7,29 @@ namespace :support do
       puts "User '#{args[:email]}' does not exist"
     end
   end
+
+  namespace :delete_user do
+    desc "Dry Run to delete user for the given email address"
+    task :dry_run, [:email] => :environment do |_, args|
+      user = OidcUser.find_by("lower(email) = ?", args[:email].downcase)
+      if user
+        puts "Dry Run: User '#{user.email}' would have been deleted"
+        puts "Dry Run: User sub: #{user.sub}"
+      else
+        puts "User '#{args[:email]}' does not exist"
+      end
+    end
+
+    desc "Delete user for the given email address"
+    task :real, [:email] => :environment do |_, args|
+      user = OidcUser.find_by("lower(email) = ?", args[:email].downcase)
+      if user
+        user.destroy!
+        puts "User '#{user.email}' deleted"
+        puts "User sub: #{user.sub}"
+      else
+        puts "User '#{args[:email]}' does not exist"
+      end
+    end
+  end
 end
--- spec/lib/support_spec.rb
+++ spec/lib/support_spec.rb
@@ -22,4 +22,44 @@ RSpec.describe "Support tasks" do
       end
     end
   end
+
+  describe ":delete_user" do
+    before do
+      FactoryBot.create(:oidc_user, email: "foo@example.gov.uk", sub: "123")
+    end
+
+    context "when a dry run" do
+      subject(:task) { Rake.application["support:delete_user:dry_run"] }
+
+      context "when a user with email exists" do
+        it "reports that the user would have been deleted" do
+          expect { task.execute({ email: "foo@example.gov.uk" }) }.to output("Dry Run: User 'foo@example.gov.uk' would have been deleted\nDry Run: User sub: 123\n").to_stdout
+        end
+
+        it "outputs other users don't exist" do
+          expect { task.execute({ email: "bar@example.com" }) }.to output("User 'bar@example.com' does not exist\n").to_stdout
+        end
+      end
+    end
+
+    context "when a real run" do
+      subject(:task) { Rake.application["support:delete_user:real"] }
+
+      context "when a user with email exists" do
+        it "reports that the user has been deleted" do
+          expect { task.execute({ email: "foo@example.gov.uk" }) }.to output("User 'foo@example.gov.uk' deleted\nUser sub: 123\n").to_stdout
+        end
+
+        it "deletes the user" do
+          count = OidcUser.count
+          task.execute({ email: "foo@example.gov.uk" })
+          expect(OidcUser.count).to eq count - 1
+        end
+
+        it "outputs other users don't exist" do
+          expect { task.execute({ email: "bar@example.com" }) }.to output("User 'bar@example.com' does not exist\n").to_stdout
+        end
+      end
+    end
+  end
 end
